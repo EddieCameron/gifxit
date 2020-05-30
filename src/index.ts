ac177f52f24fa2ca8fc9e680621d5dfe1f09dd42
Eddie Cameron <eddiec@grapefruitgames.com>
2020-05-29 20:52:55 -0400

    pickgifs

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,21 @@ import * as Slack from "./slack"
 import * as SlackActions from "./slackactions"
 import * as SteadyTimer from "./steadytimer/steadytimer"
 import { handleChooseTimeUp, handleVoteTimeUp } from "./turnmanager";
+import { query } from "./server";
+import Gif from "./models/gif";
 
 const app: express.Application = express();
 app.use(express.static('public'))
+
+app.get('/picksomegifs', async (req, res) => {
+    try {
+        const gifs = await query<Gif>(null, "SELECT * FROM gifs ORDER BY RANDOM() LIMIT 10")
+        res.json(gifs);
+    }
+    catch (e) {
+        res.status(500).send("oop");
+    }
+})
 
 Slack.init(app);
 SlackActions.init();
